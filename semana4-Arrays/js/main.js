563f39b8aa37815c79371c2f40c0e8c280fba2b7
jesusDev96 <chechujor@hotmail.com>
2026-06-02 20:59:56 -0300

    arrays, metodos de interaccion, for-of. Semana 4

---
+++ semana4-Arrays/js/main.js
@@ -0,0 +1,58 @@
+// let jugador1 = "messi"
+// let jugador2 = "dibu martinez"
+// let jugador3 = "cuti romero"
+// let jugador4 = "lautaro martinez"
+// let jugador5 = "julian alvarez"
+
+
+// const jugadores = ["messi", "dibu martinez", "cuti romero", "lautaro martinez", "julian alvarez"]
+// console.log(jugadores[1])
+// console.log(jugadores.length)
+
+// .push()
+// jugadores.push("molina")
+
+// .uhshift()
+// jugadores.unshift("paredes")
+
+// .pop()
+// jugadores.pop()
+
+// .shift()
+// jugadores.shift()
+
+// .includes()
+// console.log(jugadores.includes("molina"))
+
+// indexOf()
+// console.log(jugadores.indexOf("paredes"))
+
+// .sort()
+// jugadores.sort()
+
+// .reverse()
+// jugadores.reverse()
+
+// jugadores.sort().reverse()
+
+
+// .join()
+// console.log(jugadores.join(" - "))
+
+// .splice()
+// jugadores.splice(2, 0, "tagliafico")
+// console.log(jugadores)
+
+
+// const jugadores = ["messi", "dibu martinez", "cuti romero", "lautaro martinez", "julian alvarez"]
+
+// // for-of
+// for(const jugador of jugadores) {
+//     console.log("Jugador: " + jugador)
+// }
+
+const numeros = [1, 2, 3, 4, 5]
+for(const numero of numeros) {
+    console.log("Numero: " + numero)
+    console.log("Numero: ",numero)
+}
